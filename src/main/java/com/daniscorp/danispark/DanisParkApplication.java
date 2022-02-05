2374db18695410bd596c42e5346d440bb7bb5d35
Vinicius Lima <thelimavinicius@gmail.com>
2022-02-04 22:05:39 -0300

    Commit inicial �

---
+++ src/main/java/com/daniscorp/danispark/DanisParkApplication.java
@@ -0,0 +1,13 @@
+package com.daniscorp.danispark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanisParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanisParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniscorp/danispark/DanisParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniscorp.danispark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanisParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
