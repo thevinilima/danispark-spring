1800d63e394c33ae85e7c6a135e395e667baf1bf
Vinicius Lima <thelimavinicius@gmail.com>
2022-02-05 01:19:56 -0300

    Edit User

--- src/main/java/com/daniscorp/danispark/controllers/UsuarioController.java
+++ src/main/java/com/daniscorp/danispark/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import com.daniscorp.danispark.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,13 +18,13 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioRepository ur;
-    
-    @RequestMapping(value="/usuarios", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/usuarios", method = RequestMethod.GET)
     public ModelAndView listaUsuarios() {
         ModelAndView mv = new ModelAndView("usuarios/index");
         Iterable<Usuario> usuarios = ur.findAll();
         mv.addObject("usuarios", usuarios);
-        
+
         return mv;
     }
 
@@ -44,6 +45,31 @@ public class UsuarioController {
         return "redirect:/usuarios";
     }
 
+    @RequestMapping(value = "/usuarios/editar/{numero}", method = RequestMethod.GET)
+    public ModelAndView editarUsuario(@PathVariable("numero") int numero) {
+        Usuario usuario = ur.findByNumero(numero);
+        ModelAndView mv = new ModelAndView("/usuarios/editarForm");
+        mv.addObject("usuario", usuario);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "/usuarios/update/{numero}", method = RequestMethod.POST)
+    public String updateUsuario(
+            @PathVariable("numero") int numero,
+            @Valid Usuario usuario,
+            BindingResult result,
+            RedirectAttributes attributes
+    ) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("message", "Preencha todos os campos");
+            return "redirect:/usuarios/editar/" + numero;
+        }
+
+        ur.save(usuario);
+
+        return "redirect:/usuarios";
+    }
 
     @RequestMapping(value = "/usuarios/deletar")
     public String deletarUsuario(int numero) {
